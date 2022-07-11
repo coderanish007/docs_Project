f42df0f6db3fabb4bc9ca9db154e11de98f40795
AnishChettri <anishchettri112@gmail.com>
2022-07-12 00:24:09 +0530

    made changes to admin to make the polls app modifiable in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . models import Question, Choices
 
 # Register your models here.
+
+class ChoiceInLine(admin.StackedInline): #TabularInLine
+    model = Choices
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldset = [
+        (None, {'fields': ['question_text']}),
+        ('Date information',{'fields': ['pub_date'],'classes':['collapse']}),
+        ]
+    inlines = [ChoiceInLine]
+
+admin.site.register(Question,QuestionAdmin)
