6678b428375e492eed5dc45fce981087ba4c615a
AnishChettri <anishchettri112@gmail.com>
2022-07-20 21:54:58 +0530

    editing views detail method to raising a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from .models import Question
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[0:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html',context)
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
+   try:
+    question = Question.objects.get(pk=question_id)
+   except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+   return render(request, 'polls/results.html', { 'question': question })
+   
 def results(request, question_id):
     response = "you are looking at the result of question %s."
     return HttpResponse(response % question_id)
